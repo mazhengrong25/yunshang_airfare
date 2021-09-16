2c21c1536a8858935ce9dfae5a6c7d0d62f61739
wishQAQ <wishtime1024@gmail.com>
2021-09-16 11:25:24 +0800

    新增编码查询、修改生单逻辑、增加全局导航栏

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
  * @Description:全局配置文件
  * @Author: wish.WuJunLong
  * @Date: 2021-09-14 16:55:14
- * @LastEditTime: 2021-09-14 17:19:35
+ * @LastEditTime: 2021-09-16 10:03:11
  * @LastEditors: wish.WuJunLong
  */
 var config = (function ($) {
@@ -10,7 +10,38 @@ var config = (function ($) {
     BA: "http://192.168.0.35:6212/ba", // BA接口地址
     SQ: "http://192.168.0.35:6210/sq", // SQ接口地址
     queueplace: "http://192.168.0.35:6303/YATP_19/queueplace", // 出票地址
-    configTypeList: ["BA", "SQ"],  // 渠道选择列表
+    configTypeList: ["BA", "SQ"], // 渠道选择列表
+    header: [
+      { name: "机票查询", url: "/" },
+      { name: "编码查询", url: "/ticketCode.html" },
+      { name: "操作日志", url: "#" },
+    ],
   };
   return $;
 })(window.config || {});
+
+
+// 全局添加导航栏
+var headerUrl = "";
+for (var i = 0; i < config.airConfig.header.length; i++) {
+  headerUrl +=
+    '<a href="' +
+    config.airConfig.header[i].url +
+    '" class="' +
+    (config.airConfig.header[i].url === window.location.pathname ? "selected" : "") +
+    '">' +
+    config.airConfig.header[i].name +
+    "</a>";
+}
+$(".body").prepend(
+  "<header>" +
+    '<div class="tab_header">' +
+    '<a class="header_logo" href="/">' +
+    '<img src="/img/logo.png" alt="logo" />' +
+    "</a>" +
+    '<div class="header_tab_box">' +
+    headerUrl +
+    "</div>" +
+    "</div>" +
+    "</header>"
+);
